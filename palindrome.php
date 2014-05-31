bb31f3ca727738193f31e820e7ad925cc12efc0e
Greg Vallejo <greg.vallejo@gmail.com>
2014-05-31 14:54:52 -0500

    refactored to check multi-word strings

--- palindrome.php
+++ palindrome.php
@@ -1,45 +1,22 @@
 <?php
-// Create a function that can detect whether a word is a palindrome or not. 
-// A palindrome is a word that can be interpreted the same way in reverse order.
+function is_palindrome($string){
 
-// Create the function so it will return a bool value true if the entered word 
-// is a palindrome. Function name example could be is_palindrome(). 
-// After you are complete test several known palindromes, then test regular 
-// words in your function.
-
-function is_palindrome($word){
-
-// retrieve the word as a string
-// turn string into array
-// check to see if beginning and ending letters are same
-	$word_length = strlen($word);
-	$word_array = str_split($word);
-	$word_length_odd = ($word_length % 2); 
-// remember, arrays are zero based
-// array keys from 0 to ($word_length)-1
-	if ($word_length_odd) {
-		for (($i = 0); $i < (($word_length - 1)/2); $i++) {
-		 	if ($word_array[$i] != $word_array[$word_length - ($i+1)]){
-				return false;
-				break;
-			} else {
-				return true;
-		}
-	}
-	} else {
-		for ($i = 0; $i < ($word_length/2); $i++) {
-			if ($word_array[$i] != $word_array[$word_length - ($i+1)]){
-				return false;
-				break;
-			} else {
-				return true;
-			}
-		}
-	}
+	//2.0
+	//make string lower case, to ignore case sensitivity
+	$string = strtolower($string);
+	//take sentence, strip non-letter characters
+	$string = str_replace($string, ' ', '.,;:"\'-');
+	//turn string into array
+	$backwards = explode(' ', $string);
+	//reverse array
+	$sdrawkcab = rsort($backwards);
+	//check if arrays are the same
+	return ($backwards == $sdrawkcab);
 }
 
-$word = 'tattarrattat';
-if (is_palindrome($word)) {
+echo "Enter a sentnce, I'll check if it's a palindrome:".PHP_EOL;
+$string = trim(fgets(STDIN));
+if (is_palindrome($string)) {
 	echo 'This is a palindrome'.PHP_EOL;
 } else {
 	echo 'This is not a palindrome'.PHP_EOL;
